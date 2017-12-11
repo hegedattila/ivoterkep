d248ab79b29b596d31b1cc55d3043c33dfb81016
Gergely Pintér <gergelyp0@gmail.com>
2017-12-11 13:11:51 +0100

    updates

--- System/ProjectManager.php
+++ System/ProjectManager.php
@@ -3,8 +3,9 @@
 namespace System;
 class ProjectManager {
     public static function getProjectName(){
-        $arr = [];
+        /*$arr = [];
         preg_match('/([\w-_]+).[\w]+$/', $_SERVER['HTTP_HOST'], $arr);
-        return $arr[1];
+        return $arr[1];*/
+        return 'it';
     }
 }
